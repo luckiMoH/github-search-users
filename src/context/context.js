0503c313ba0e41815df8749b7931266a845619ed
Mateusz Majewski <mat.maj1992@gmail.com>
2023-05-09 20:00:26 +0200

    functionality to fetch data from api works

--- src/context/context.js
+++ src/context/context.js
@@ -1,21 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
 
 const GithubContext = React.createContext();
 
 // Provider, Consumer - GithubContext.Provider
 
-const GithubProvider = ({children}) => {
-    const [githubUser, setGithubUser] = useState(mockUser)
-    const [githubRepos, setGithubRepos] = useState(mockRepos)
-    const [githubFollowers, setGithubFollowers] = useState(mockFollowers)
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [githubRepos, setGithubRepos] = useState(mockRepos);
+  const [githubFollowers, setGithubFollowers] = useState(mockFollowers);
+  // request, loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  // error
+  const [error, setError] = useState({ show: false, msg: "" });
 
-    return <GithubContext.Provider value={{githubUser, githubRepos, githubFollowers}}>{children}</GithubContext.Provider>
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      //repos
+      axios(`${rootUrl}/users/${login}/repos?per_page100`).then((response) =>
+        setGithubRepos(response.data)
+      );
+      //followers
+      axios(`${followers_url}?per_page100`).then((response) =>
+        setGithubFollowers(response.data)
+      );
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+    checkRequest();
+    setIsLoading(false);
+  };
+
+  // check rate
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "sorry, you have exceeded your hourly rate limit!");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  useEffect(checkRequest, []);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        githubRepos,
+        githubFollowers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
 };
 
 export { GithubContext, GithubProvider };
